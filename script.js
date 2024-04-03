bb0d8f693e81965cf9ba27a4d58b385f9910a972
RunningImp14 <99064020+IgnacioAlcalde@users.noreply.github.com>
2024-04-03 19:55:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const heart = document.querySelector('.heart');
+
+  // Agrega una clase al corazón cuando se carga el documento para iniciar la animación
+  heart.classList.add('animateHeart');
+});
